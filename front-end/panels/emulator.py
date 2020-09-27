db17f9b450ef45a46107b3943658dd929a5460e1
Nathan Peters <natesurf3@yahoo.com>
2020-09-27 17:03:59 -0600

    exiting the emulator

--- front-end/panels/emulator.py
+++ front-end/panels/emulator.py
@@ -4,6 +4,8 @@ from hardware.keyboard import Keyboard
 from hardware.screen import Screen
 from hardware.sound import Sound
 
+from panels import welcome
+
 class Emulator:
 	def __init__(self):
 		self.communicator = Communicator()
@@ -30,4 +32,8 @@ class Emulator:
 		for e in events:
 			if e.type == pygame.QUIT:
 				panels.clear()
+			elif e.type == pygame.KEYUP:
+				if e.key == pygame.K_ESCAPE:
+					panels.clear()
+					panels.append(welcome.Welcome())
 		self.screen.draw(surf)
